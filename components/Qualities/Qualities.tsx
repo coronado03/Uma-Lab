5b92b4065a341b4629049a206f5afd8bfd522fd7
coronado03 <sebascoronado03@hotmail.com>
2023-06-24 22:13:52 +0200

    initial qualities_section commit

--- components/Qualities/Qualities.tsx
+++ components/Qualities/Qualities.tsx
@@ -2,33 +2,96 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
 
-function Footer() {
+function Qualities() {
   
+    const [isClicked, setIsClicked] = useState([false, false, false]);
+
+    const handleClick = (index:number) => {
+        const updatedIsClicked = [...isClicked];
+        updatedIsClicked[index] = !updatedIsClicked[index];
+        setIsClicked(updatedIsClicked);
+      };
+
+
+
   return (
-    <div className='flex bg-white'>
+    <div className='flex bg-[#EEEEEE] text-black'>
+    
+    <div className='flex flex-col gap-y-0.5 w-3/5 mx-auto my-20'>
 
-        <div className='flex flex-row self-center'>
-
-            <Image
-            src="/Footer/facebook.png"
-            width={40}
-            height={110}
-            alt="logo"
-            className="rounded-full"/>
+        <div className='flex flex-col py-6 px-10 bg-white group transition-all duration-500 ease-in'>
 
 
-            <Image
-            src="/Footer/instagram.png"
-            width={40}
-            height={110}
-            alt="logo"
-            className="rounded-full"/>
+            <div className='flex flex-row items-center justify-between'>
+                <h1 className='font-semibold text-2xl'>High Quality</h1>
+                <button
+                className='rounded-full p-1.5 group-hover:bg-[#EEEEEE] transition-all'
+                onClick={()=> handleClick(0)}>
+                <Image src="/close.png" className={isClicked[0] ? 'transition-all' : 'transition-all rotate-45'} width={30} height={30} alt="logo" />
+
+                </button>
+            </div>
+                <p 
+                className={isClicked[0]  ? 'mt-4 transition-all' : 'transition-all hidden'}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Integer dapibus elit quis risus viverra, vitae viverra quam
+                fringilla. Nunc ut ligula nec metus volutpat aliquam. Fusce
+                finibus aliquam enim sed eleifend.
+                </p>
 
         </div>
+
+        <div className='flex flex-col py-6 px-10 bg-white group transition-all duration-500 ease-in'>
+
+
+            <div className='flex flex-row items-center justify-between'>
+                <h1 className='font-semibold text-2xl'>High Quality</h1>
+                <button
+                className='rounded-full p-1.5 group-hover:bg-[#EEEEEE] transition-all'
+                onClick={()=> handleClick(1)}>
+                <Image src="/close.png" className={isClicked[1] ? 'transition-all' : 'transition-all rotate-45'} width={30} height={30} alt="logo" />
+
+                </button>
+            </div>
+                <p 
+                className={isClicked[1]  ? 'mt-4 transition-all' : 'transition-all hidden'}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Integer dapibus elit quis risus viverra, vitae viverra quam
+                fringilla. Nunc ut ligula nec metus volutpat aliquam. Fusce
+                finibus aliquam enim sed eleifend.
+                </p>
+
+        </div>
+
+        <div className='flex flex-col py-6 px-10 bg-white group transition-all duration-500 ease-in'>
+
+
+            <div className='flex flex-row items-center justify-between'>
+                <h1 className='font-semibold text-2xl'>High Quality</h1>
+                <button
+                className='rounded-full p-1.5 group-hover:bg-[#EEEEEE] transition-all'
+                onClick={()=> handleClick(2)}>
+                <Image src="/close.png" className={isClicked[2] ? 'transition-all' : 'transition-all rotate-45'} width={30} height={30} alt="logo" />
+
+                </button>
+            </div>
+                <p 
+                className={isClicked[2]  ? 'mt-4 transition-all' : 'transition-all hidden'}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Integer dapibus elit quis risus viverra, vitae viverra quam
+                fringilla. Nunc ut ligula nec metus volutpat aliquam. Fusce
+                finibus aliquam enim sed eleifend.
+                </p>
+
+        </div>
+
+
+    </div>
+
 
     </div>
   );
 }
 
-export default Footer;
+export default Qualities;
 
